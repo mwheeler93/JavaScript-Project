e32804898386a59e7948b1cf96d456e16e055881
mwheeler93 <46714208+mwheeler93@users.noreply.github.com>
2019-04-14 23:56:57 -0400

    Add files via upload
    Still playing with template literals; hard to tell where the limits are at the moment. Need to look at documentation so I can begin making more interesting dynamic elements.

--- profile.js
+++ profile.js
@@ -1,9 +1,17 @@
 var DynamicButton = document.createElement("button");
 DynamicButton.className = "DynamicButton";
 DynamicButton.innerHTML = "Click Me!"
+var clicks = 0;
 DynamicButton.onclick = function() {
-  var clicks = 0;
   clicks++;
   DynamicButton.backgroundColor = `rgb(${clicks}%,${clicks}%,${clicks}%)`;
 };
 document.body.appendChild(DynamicButton);
+var c = 0
+var TestButton = document.createElement("button");
+TestButton.innerHTML = "Test"
+TestButton.onclick = function() {
+  c += 10;
+  TestButton.backgroundColor = `rgb(${c}%), (${c}%), (${c}%)`
+}
+document.body.appendChild(TestButton);
